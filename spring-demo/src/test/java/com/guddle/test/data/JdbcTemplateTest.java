e1a9299f033f110ee3f176b9cacc2a79b1a172ff
guddle <yangguoduo@126.com>
2014-09-25 11:45:17 +0800

    add spring mvc

---
+++ spring-demo/src/test/java/com/guddle/test/data/JdbcTemplateTest.java
@@ -0,0 +1,42 @@
+package com.guddle.test.data;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ContextConfiguration;
+
+
+@ContextConfiguration(locations = { "/applicationContext-test.xml" })
+public class JdbcTemplateTest extends SpringTransactionalTestCase {
+
+	private static Logger log = LoggerFactory.getLogger(JdbcTemplateTest.class);
+	@PersistenceContext
+	private EntityManager entityManager;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	@Test
+	public void testSqlQuery(){
+		
+		String sql = "select 1";
+		/*
+		 * PersisternceContext 测试
+		 */
+		assertNotNull(entityManager);
+		
+		/*
+		 * JDBC测试
+		 */
+		int i = jdbcTemplate.queryForObject(sql, Integer.class);
+		
+		log.debug("@JdbcTemplate查询结果：第一个部署的流程名称是："+i);
+	}
+}
